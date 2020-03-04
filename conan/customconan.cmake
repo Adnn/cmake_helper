5075912aaa8b7eddb3f67794c274eaac65f6b720
Adrien <adrien@astutegraphics.com>
2020-03-04 16:19:16 +0100

    customconan.cmake to handle CMAKE_MSVC_RUNTIME_LIBRARY

--- conan/customconan.cmake
+++ conan/customconan.cmake
@@ -14,6 +14,11 @@ macro(conan_handle_compiler_settings)
     conan_set_std()
     conan_set_libcxx()
     conan_set_vs_runtime()
+    # Since 3.15, setting the runtime flags explicitly does not work anymore
+    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+    if(CONAN_LINK_RUNTIME IN_LIST "/MD;/MDd")
+            set(CMAKE_MSVC_RUNTIME_LIBRARY "${CMAKE_MSVC_RUNTIME_LIBRARY}DLL")
+    endif()
 endmacro()
 
 include(${CMAKE_BINARY_DIR}/conan_paths.cmake)
