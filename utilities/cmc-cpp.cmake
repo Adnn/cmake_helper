0904565a526e93c6fa56ac1d8677ba0b46af78dd
Adrien <Adnn@users.noreply.github.com>
2022-10-06 19:13:06 +0200

    Add sanitizer option control (only Address or None atm).

--- utilities/cmc-cpp.cmake
+++ utilities/cmc-cpp.cmake
@@ -1,20 +1,53 @@
 ## \brief Enable all warnings as error for provided TARGET.
 ##        Additionally raises the warning level except for MSVC.
 function(cmc_cpp_all_warnings_as_errors TARGET)
+    set(oneValueArgs "ENABLED")
+    cmake_parse_arguments(CAS "${optionsArgs}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    set(gnuoptions "AppleClang" "Clang" "GNU")
-    if (CMAKE_CXX_COMPILER_ID IN_LIST gnuoptions)
-        set(option "-Werror")
-        # Enables all warnings
-        # Except on MSVC, where all warnings create too many problems with system headers
-        target_compile_options(${TARGET} PRIVATE -Wall)
-    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        set(option "-WX")
+    # If the optional one-value-arg ENABLED is omitted, warning_as_error is also enabled.
+    if (NOT DEFINED CAS_ENABLED OR CAS_ENABLED)
+        set(gnuoptions "AppleClang" "Clang" "GNU")
+        if (CMAKE_CXX_COMPILER_ID IN_LIST gnuoptions)
+            set(option "-Werror")
+            # Enables all warnings
+            # Except on MSVC, where all warnings create too many problems with system headers
+            target_compile_options(${TARGET} PRIVATE "-Wall")
+        elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+            set(option "-WX")
+            target_compile_options(${TARGET} PRIVATE "-W3")
+        endif()
+
+        # The option is given as both compile option and link option
+        # Note:: target_link_options is only available from 3.13
+        target_compile_options(${TARGET} PRIVATE ${option})
+        target_link_libraries(${TARGET} PRIVATE ${option})
     endif()
 
-    # The option is given as both compile option and link option
-    # Note:: target_link_options is only available from 3.13
-    target_compile_options(${TARGET} PRIVATE ${option})
-    target_link_libraries(${TARGET} PRIVATE ${option})
+endfunction()
 
+
+## \brief Define a CMake variable containing the enumeration of available sanitizers.
+function(cmc_cpp_define_sanitizer_enum)
+    set(sanitizers "None" "Address")
+    set(BUILD_CONF_Sanitizer "None" CACHE STRING "Enable a sanitizer.")
+    set_property(CACHE BUILD_CONF_Sanitizer PROPERTY STRINGS ${sanitizers})
+endfunction()
+
+
+## \brief Enable (Clang) sanitizers.
+function(cmc_cpp_sanitizer TARGET TYPE)
+    if (TYPE STREQUAL "None")
+        # nothing to do here
+    elseif (TYPE STREQUAL "Address")
+        set(gnuoptions "AppleClang" "Clang" "GNU")
+        if (CMAKE_CXX_COMPILER_ID IN_LIST gnuoptions)
+            target_compile_options(${TARGET_NAME} PRIVATE /fsanitize=address)
+            target_link_options(${TARGET_NAME} PRIVATE /fsanitize=address)
+        elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+            target_compile_options(${TARGET_NAME} PRIVATE /fsanitize=address)
+            target_link_options(${TARGET_NAME} PRIVATE /fsanitize=address)
+        endif()
+    else()
+        message(SEND_ERROR "Sanitizer type '${TYPE}' not supported. Aborting.")
+    endif()
 endfunction()
